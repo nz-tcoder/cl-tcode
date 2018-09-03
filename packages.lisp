7ed3a121b11bc5a0d1e3f720720fc8bc200e40eb
nz-tcoder <nz.tcoder@gmail.com>
2018-09-04 08:28:43 +0900

    export :tc-jis-code-select-mode

--- packages.lisp
+++ packages.lisp
@@ -2,4 +2,5 @@
   (:use :cl :lem)
   (:export :tc-mode :wj :char-to-string :setup-tcode
            :tcode-display-help-buffer :make-stroke-help
+           :tc-jis-code-select-mode
            :tc-mazegaki-mode :set-mazegaki-dic :make-mazegaki-dic))
